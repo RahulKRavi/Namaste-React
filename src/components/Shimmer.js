3d27bc6a738ea79d287cbcb510a2f2f4e911ce8b
rahulkravi <rahulkr02042000@gmail.com>
2024-08-01 18:53:23 +0530

    Add shimmer to the components

---
+++ src/components/Shimmer.js
@@ -0,0 +1,26 @@
+const Shimmer = () => {
+    return (
+        <>
+            <div className="header">
+                <ul className="navbar-list">
+                    <li className="navbar-items"></li>
+                    <li className="navbar-items"></li>
+                    <li className="navbar-items"></li>
+                </ul>
+            </div>
+
+            <div className="body">
+                <div className="card">
+                    <img className="card-img"></img>
+                    <div className="container">
+                        <h4><b></b></h4>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
+export default Shimmer;
